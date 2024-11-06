5ab9de83a24556d567e7ca4eb80df3debe65050d
asp <asp@aptech.com>
2024-11-06 13:04:29 +0500

    first

---
+++ WebApplication08/WebApplication08/Controllers/HomeController1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication08.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["designation"] = "Softeware developer";
+                TempData.Keep();
+
+            ViewBag.name = "Muhammad Taha";
+            ViewData["Salary"] = "Your Salary Is 8$ Billion ";
+            ViewData["Designation"] = "CEO of the Company";
+
+            return View();
+        }
+
+
+        public IActionResult Airbuds()
+        {
+            TempData.Keep();
+
+            return View();
+
+        }
+
+        public IActionResult Laptop() 
+        {
+            TempData.Keep();
+            return View();
+        }
+
+        public IActionResult Mobiles()
+        {
+            TempData.Keep();
+            return View();
+        }
+    }
+}
