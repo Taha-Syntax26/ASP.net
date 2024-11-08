db30cc6b1099934d5afa22562f15c04693e4f9f9
asp <asp@aptech.com>
2024-11-08 12:32:37 +0500

    login

--- WebApplication08/WebApplication08/Controllers/HomeController1.cs
+++ WebApplication08/WebApplication08/Controllers/HomeController1.cs
@@ -19,6 +19,8 @@ namespace WebApplication08.Controllers
 
         public IActionResult Airbuds()
         {
+            TempData["Brand"] = "Apple";
+
             TempData.Keep();
 
             return View();
---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication2.Models;
+
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(string email, string pswd)
+        {
+            if (email == "admin@gmail.com" && pswd == "aptech123")
+            {
+                return Content("Thankyou Login Successfully");
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
