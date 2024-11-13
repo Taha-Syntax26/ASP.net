bbf2b6e85a2c86fa2268468c2eba1327da69b839
asp <asp@aptech.com>
2024-11-13 12:43:05 +0500

    2

---
+++ templateembedding2/templateembedding2/Controllers/my.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace templateembedding2.Controllers
+{
+    public class my : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Index(string firstName, string lastName)
+        {
+            ViewBag.Name = string.Format("Name:{0} {1}", firstName, lastName);
+            return View();
+        }
+    }
+
+}
