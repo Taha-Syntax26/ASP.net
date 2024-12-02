3e928a23c000a63db55fb3f06581526065d0185d
tahaa-syntax2608 <mtaha260804@gmail.com>
2024-12-02 11:03:32 +0500

    DB

--- DBfirstApproach/Controllers/HomeController.cs
+++ DBfirstApproach/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using DBfirstApproach.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Diagnostics;
 
 namespace DBfirstApproach.Controllers
@@ -18,6 +19,54 @@ namespace DBfirstApproach.Controllers
         {
             return View(db.Products.ToList());
         }
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult EditProduct(int id)
+        {
+            var product = db.Products.FirstOrDefault(x => x.Id == id);
+            return View();
+        }
+        [HttpPost]
+         public IActionResult EditProduct(Product product)
+        {
+            db.Products.Update(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteProduct(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DeleteProduct(Product product)
+        {
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult ReadProduct(int id)
+        {
+            var product = db.Products.FirstOrDefault(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();  
+            }
+
+            return View(product);  
+        }
 
         public IActionResult Privacy()
         {
@@ -30,4 +79,6 @@ namespace DBfirstApproach.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+
+    
 }
